7b38f8f185471f5c0017da17f1c83f48d95553a3
LeeKharHoongNhat <36950051+LeeKharHoongNhat@users.noreply.github.com>
2019-03-04 19:51:51 +0700

    up database

--- database/quanlyduan.sql
+++ database/quanlyduan.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 02, 2019 at 05:22 AM
+-- Generation Time: Mar 04, 2019 at 01:50 PM
 -- Server version: 5.6.11
 -- PHP Version: 5.5.3
 
@@ -58,9 +58,19 @@ INSERT INTO `cong_viec_du_an` (`id`, `ma_du_an`, `ma_cong_viec`, `noi_dung`, `ma
 --
 
 CREATE TABLE IF NOT EXISTS `danh_muc_du_an` (
-  `ma_du_an` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
-  `ma_tieu_thuc` varchar(11) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ma_du_an` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ma_tieu_thuc` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `danh_muc_du_an`
+--
+
+INSERT INTO `danh_muc_du_an` (`id`, `ma_du_an`, `ma_tieu_thuc`) VALUES
+(1, 'DA001', 'CD'),
+(2, 'DA001', 'CD1');
 
 -- --------------------------------------------------------
 
@@ -111,7 +121,7 @@ CREATE TABLE IF NOT EXISTS `khach_hang` (
   `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `ma_so_thue` varchar(13) COLLATE utf8_unicode_ci NOT NULL,
   `so_tai_khoan` int(13) NOT NULL,
-  `ghi_chu` varchar(500) COLLATE utf8_unicode_ci NOT NULL
+  `ghi_chu` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -120,7 +130,8 @@ CREATE TABLE IF NOT EXISTS `khach_hang` (
 
 INSERT INTO `khach_hang` (`ma_khach_hang`, `ten_khach_hang`, `dia_chi`, `so_dien_thoai`, `email`, `ma_so_thue`, `so_tai_khoan`, `ghi_chu`) VALUES
 ('KH001', 'Lê Khả Duy', 'Mỹ Đình, Hà Nội', 397804698, 'lekhaduaT31@gmail.com', '', 2147483647, ''),
-('KH002', 'Cty Fomosa ', 'Nha Trang, Khánh Hòa', 975661206, 'Fomosavn@gmail.com', '12345678', 2147483647, 'Đại diện: Nguyễn Minh Khoa');
+('KH002', 'Cty Fomosa ', 'Nha Trang, Khánh Hòa', 975661206, 'Fomosavn@gmail.com', '12345678', 2147483647, 'Đại diện: Nguyễn Minh Khoa'),
+('KH003', 'Lê Khả Hồng Nhật', 'Thanh Hóa', 978987678, 'lekhahongnhatzp1998@gmail.com', '343433444', 123423422, NULL);
 
 -- --------------------------------------------------------
 
@@ -255,6 +266,33 @@ CREATE TABLE IF NOT EXISTS `tien_do_du_an` (
 INSERT INTO `tien_do_du_an` (`ma_cong_viec`, `ma_nhan_vien`, `noi_dung`, `danh_gia_nhan_vien`, `danh_gia_pm`) VALUES
 ('CV1', 'NV001', 'Xây dựng toàn bộ database dự án. tiến độ đúng hạn', 10, 5),
 ('CV1A', 'NV001', 'xây dựng database khách hàng hoàn thành', 100, 100);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vat_lieu`
+--
+
+CREATE TABLE IF NOT EXISTS `vat_lieu` (
+  `ma_cong_viec` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `so_luong` int(5) NOT NULL,
+  `don_gia` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `chi_phi` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `ghi_chu` text COLLATE utf8_unicode_ci NOT NULL,
+  `is_delete` tinyint(4) NOT NULL,
+  PRIMARY KEY (`ma_cong_viec`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `vat_lieu`
+--
+
+INSERT INTO `vat_lieu` (`ma_cong_viec`, `so_luong`, `don_gia`, `chi_phi`, `ghi_chu`, `is_delete`) VALUES
+('CV01', 10, '2555', '255000', '1 tuần', 1),
+('CV02', 2, '200', '2000', 'JJJ', 0),
+('CV03', 2, '2000', '20000', 'TEXT', 0),
+('CV04', 2, '2000', '6000', 'ee', 0),
+('CV05', 2, '2000', '4000', 'HHMM', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
